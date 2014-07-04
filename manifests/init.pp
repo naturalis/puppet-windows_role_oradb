0d9fe9cf2fd165ad7b9620c5f17af9132da022af
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-07-04 09:15:00 -0700

    Modifications

--- manifests/init.pp
+++ manifests/init.pp
@@ -51,6 +51,7 @@ class windows_role_oradb  (
 
   $database_hash  = {'nbcprod' => { # General
                                     oracleHome     => 'C:\Oracle_Sys\nbcprod\product\11.2\db',
+                                    oracleBase     => 'C:\Oracle_Sys\nbcprod',
                                     version        => '11.2.0.3',
                                     installFolder  => 'C:/Install',
 
